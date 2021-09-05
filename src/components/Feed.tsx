bd97e5ac3f6511878bd3b16239c72df7fbd383dd
Hiroki Sato <raxito2008@yahoo.co.jp>
2021-09-05 11:58:03 +0900

    add カテゴリ別出力の実装

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase";
-// import { useSelector } from "react-redux";
-// import { selectUser } from "../features/userSlice";
 import TweetInput from "./TweetInput";
 import Post from "./Post";
 
 const Feed: React.FC = () => {
-  // const user = useSelector(selectUser);
   const [posts, setPosts] = useState([
     {
       id: "",
@@ -45,6 +42,17 @@ const Feed: React.FC = () => {
       unSub();
     };
   }, []);
+
+  const posts_filter = (cat: string) => {
+    const result = posts.filter((post) => {
+      return post.category === cat;
+    });
+    return result;
+  };
+
+  const posts_guitar = posts_filter("guitar");
+  const posts_bass = posts_filter("bass");
+
   return (
     <>
       <TweetInput />
@@ -72,9 +80,31 @@ const Feed: React.FC = () => {
       </section>
       <section className="mt-8 mb-8">
         <h2 className="text-lg">#guitar</h2>
-        {posts[0]?.id && (
+        {posts_guitar[0]?.id && (
           <ul className="flex flex-wrap -m-2 mt-0">
-            {posts.map((post) => (
+            {posts_guitar.map((post) => (
+              <Post
+                key={post.id}
+                postId={post.id}
+                avatar={post.avatar}
+                image={post.image}
+                category={post.category}
+                text={post.text}
+                brandName={post.brandName}
+                gearName={post.gearName}
+                timestamp={post.timestamp}
+                username={post.username}
+                likeCount={post.likeCount}
+              />
+            ))}
+          </ul>
+        )}
+      </section>
+      <section className="mt-8 mb-8">
+        <h2 className="text-lg">#bass</h2>
+        {posts_bass[0]?.id && (
+          <ul className="flex flex-wrap -m-2 mt-0">
+            {posts_bass.map((post) => (
               <Post
                 key={post.id}
                 postId={post.id}
